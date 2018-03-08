cfb8c0b7e49f35dd433000c56cf186d92020fc73
Chris Farman <cf889@york.ac.uk>
2018-03-08 16:55:24 +0000

    Fixed Colin-Skip/Tutorial-Skip in CS

---
+++ docs/search/variables_8 (1).js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main',['main',['../class_quest.html#ab212e8753c273c4619f99e5fc28928a5',1,'Quest']]],
+  ['money',['money',['../class_global_functions.html#a99a86396298d0e84bb8b0ae01d6dcb75',1,'GlobalFunctions']]],
+  ['moveplayer',['movePlayer',['../class_scene_changer.html#a3b204c06327370f2f5289b5a97b73b23',1,'SceneChanger']]]
+];
